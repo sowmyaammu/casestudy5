3d807ae48566ea3703f52df2504970a5b2355d5d
Training_C2a.04.28 <Training_C2a.04.28@CDC2-D-47B4N62.dir.svc.accenture.com>
2019-09-06 11:04:20 +0530

    fdrrt

---
+++ src/cucumber/c.java
@@ -0,0 +1,14 @@
+package cucumber;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class c {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
